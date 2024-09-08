a85cecb15f20a52b15cdde371f6ba5e8254dbccf
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-08 14:56:43 +0300

    updated: let & also

--- app/src/main/java/com/ismailmesutmujde/kotlinadvancedfunctions/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinadvancedfunctions/MainActivity.kt
@@ -5,9 +5,39 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+
+    private var myInt : Int? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if(myInt != null) {
+            val num = myInt!! + 1
+        }
+
+        myInt?.let {
+            val num = it + 1
+        }
+
+        /*
+        val myNum = myInt?.let {
+            it + 1
+        }?:0
+         */
+
+        // also
+        val ismail = Person("ismail", 35)
+        val mesut = Person("mesut", 10)
+        val mujde = Person("mujde",25)
+
+        val people = listOf<Person>(ismail,mesut,mujde)
+        people.filter { it.age > 18 }.also {
+            for(person in it) {
+                println(person.name)
+            }
+        }
+        
     }
 }
+
+data class Person(var name: String, var age: Int)
